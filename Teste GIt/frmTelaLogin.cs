d9f93b3fe34a008032916a054704b5b6fde16100
matxeyc <matxeyclima@gmail.com>
2017-03-15 15:46:20 -0300

    Base mvc definida

---
+++ Teste GIt/frmTelaLogin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TesteGit.DTO;
+
+namespace Teste_GIt
+{
+    public partial class frmTelaLogin : Form
+    {
+        public frmTelaLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TesteGit.DTO/UsuarioDTO.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteGit.DTO
+{
+    public class UsuarioDTO
+    {
+        private String nome;
+        private String email;
+        private String senha;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string Senha
+        {
+            get
+            {
+                return senha;
+            }
+
+            set
+            {
+                senha = value;
+            }
+        }
+    }
+}
